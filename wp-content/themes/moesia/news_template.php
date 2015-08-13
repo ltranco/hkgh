5c3c350a510de704a29939373d1b43d80bf21053
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-13 16:01:34 -0700

    trying to add custom template

--- wp-content/themes/moesia/news_template.php
+++ wp-content/themes/moesia/news_template.php
@@ -1,1 +1,38 @@
-<?php /* Template Name: News Template */ ?>
+<?php /* Template Name: News Template */
+	get_header();
+?>
+
+	<div id="primary" class="content-area fullwidth">
+		<main id="main" class="site-main" role="main">
+
+			<?php 
+    				query_posts(array( 
+        				'post_type' => 'portfolio',
+        				'showposts' => 10 
+    				));  
+			?>
+			
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+				
+				<?php
+
+					// If comments are open or we have at least one comment, load up the comment template
+
+					if ( comments_open() || '0' != get_comments_number() ) :
+
+						comments_template();
+
+					endif;
+
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
