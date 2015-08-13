11c5cbaf2e792b68f8b037c70d2c8213f9283b42
Long Tran <v.long128@gmail.com>
2015-08-12 20:31:06 -0700

    configing post

--- wp-content/themes/moesia/content-single.php
+++ wp-content/themes/moesia/content-single.php
@@ -14,10 +14,6 @@
 
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php moesia_posted_on(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- wp-content/themes/moesia/single.php
+++ wp-content/themes/moesia/single.php
@@ -9,7 +9,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php if ( !is_singular( 'projects' ) ): ?>
@@ -24,5 +23,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
