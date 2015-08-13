31d9baa83a20b95ec0078da0cd0edee0048f1ba6
Long Tran <v.long128@gmail.com>
2015-08-12 20:33:29 -0700

    configing post

--- wp-content/themes/moesia/single.php
+++ wp-content/themes/moesia/single.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area" style="width:80%;">
 		<main id="main" class="site-main" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
 
