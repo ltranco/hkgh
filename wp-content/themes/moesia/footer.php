fe175b8b5b8130d27012425d3751cfe9835ea61d
Long Tran <v.long128@gmail.com>
2015-08-14 00:14:07 -0700

    footer

--- wp-content/themes/moesia/footer.php
+++ wp-content/themes/moesia/footer.php
@@ -16,7 +16,7 @@
 	<?php if ( is_active_sidebar( 'sidebar-3' ) || is_active_sidebar( 'sidebar-4' ) || is_active_sidebar( 'sidebar-5' ) ) : ?>
 		<?php get_sidebar('footer'); ?>
 	<?php endif; ?>
-	<footer id="colophon" class="site-footer" role="contentinfo" style="height:73px">
+	<footer id="colophon" class="site-footer" role="contentinfo" style="height:53px">
 		<?php tha_footer_top(); ?>
 		<div class="site-info container">
 			<p style="color:white">Homeless Gay Kids - Houston | P.O. Box 980935, Houston, Texas 77098 | <a id="#footerLink" style="color:white;" href="mailto:secretary@HomelessGayKidsHouston.org">secretary@HomelessGayKidsHouston.org</a> | <a style="text-decoration:none;" href="https://www.facebook.com/pages/Homeless-Gay-Kids-Houston/1479108399056136?fref=ts"><img height="32" width="32" src="http://hkgh.lvtran.com/wp-content/themes/moesia/images/fb.svg"/></a></p>
