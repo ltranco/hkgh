fd261f9b20feea35b1362a9e3f817822dc482a1a
Long Tran <v.long128@gmail.com>
2015-08-14 00:16:00 -0700

    header

--- wp-content/themes/moesia/styles.php
+++ wp-content/themes/moesia/styles.php
@@ -317,7 +317,7 @@ function moesia_custom_styles($custom) {
 	}
 	//Menu background
 	$menu_bg = esc_html(get_theme_mod( 'menu_color' ));
-	$custom .= ".top-bar { background-color: #5bb82c; height: 90px;}"."\n";
+	$custom .= ".top-bar { background-color: #5bb82c; height: 125px;}"."\n";
 
 	//Menu links
 	$menu_links_color = esc_html(get_theme_mod( 'menu_links_color' ));
