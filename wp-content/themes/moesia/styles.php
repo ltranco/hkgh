1f6aac261ac54469cf3f0b47a9922d080a2d1490
Long Tran <v.long128@gmail.com>
2015-08-15 00:43:54 -0700

    lowering navbar height

--- wp-content/themes/moesia/styles.php
+++ wp-content/themes/moesia/styles.php
@@ -317,7 +317,7 @@ function moesia_custom_styles($custom) {
 	}
 	//Menu background
 	$menu_bg = esc_html(get_theme_mod( 'menu_color' ));
-	$custom .= ".top-bar { background-color: #84a939; height: 100px;}"."\n";
+	$custom .= ".top-bar { background-color: #84A939; height: 100px;}"."\n";
 
 	//Menu links
 	$menu_links_color = esc_html(get_theme_mod( 'menu_links_color' ));
