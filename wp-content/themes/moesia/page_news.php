52238b7f8cecc73af17c9adcd589dd7ed650a7de
Long Tran <v.long128@gmail.com>
2015-08-15 22:16:26 -0700

    trying to query specific posts

--- wp-content/themes/moesia/page_news.php
+++ wp-content/themes/moesia/page_news.php
@@ -13,6 +13,14 @@ Template Name: Full width
 	<div id="primary" class="content-area fullwidth">
 		<main id="main" class="site-main" role="main">
 
+			<?php
+			if ( get_query_var('paged') ) $paged = get_query_var('paged');  
+			if ( get_query_var('page') ) $paged = get_query_var('page');
+ 
+			$query = new WP_Query( array( 'post_type' => 'news', 'paged' => $paged ) );
+ 
+			if ( $query->have_posts() ) : ?>
+
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class="entry">
 				<h2 class="title"><?php the_title(); ?></h2>
