954ba152bf95822df755ec37b08186395954e7cf
Long Tran <v.long128@gmail.com>
2015-08-15 22:07:59 -0700

    trying to query specific posts

--- wp-content/themes/moesia/page_fullwidth.php
+++ wp-content/themes/moesia/page_fullwidth.php
@@ -13,35 +13,23 @@ Template Name: Full width
 	<div id="primary" class="content-area fullwidth">
 		<main id="main" class="site-main" role="main">
 
-			<?php 
-    $args = array(
-	'show_option_all'    => '',
-	'orderby'            => 'name',
-	'order'              => 'ASC',
-	'style'              => 'list',
-	'show_count'         => 0,
-	'hide_empty'         => 1,
-	'use_desc_for_title' => 1,
-	'child_of'           => 0,
-	'feed'               => '',
-	'feed_type'          => '',
-	'feed_image'         => '',
-	'exclude'            => '',
-	'exclude_tree'       => '',
-	'include'            => '',
-	'hierarchical'       => 1,
-	'title_li'           => __( 'Categories' ),
-	'show_option_none'   => __( '' ),
-	'number'             => null,
-	'echo'               => 1,
-	'depth'              => 0,
-	'current_category'   => 0,
-	'pad_counts'         => 0,
-	'taxonomy'           => 'category',
-	'walker'             => null
-    );
-    wp_list_categories( $args ); 
-?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php
+
+					// If comments are open or we have at least one comment, load up the comment template
+
+					if ( comments_open() || '0' != get_comments_number() ) :
+
+						comments_template();
+
+					endif;
+
+				?>
+
+			<?php endwhile; // end of the loop. ?>
 
 
 		</main><!-- #main -->
--- wp-content/themes/moesia/page_news.php
+++ wp-content/themes/moesia/page_news.php
@@ -2,7 +2,7 @@
 
 /*
 
-Template Name: News
+Template Name: Full width
 
 */
 	get_header();
@@ -12,23 +12,44 @@ Template Name: News
 
 	<div id="primary" class="content-area fullwidth">
 		<main id="main" class="site-main" role="main">
-			<?php
-				if ( get_query_var('paged') ) $paged = get_query_var('paged');  
-				if ( get_query_var('page') ) $paged = get_query_var('page');
- 
-				$query = new WP_Query( array( 'post_type' => 'news', 'paged' => $paged ) );
-				 
-				if ( $query->have_posts() ) : ?>
-					<?php while ( $query->have_posts() ) : $query->the_post(); ?>	
-						<div class="entry">
-							<h2 class="title"><?php the_title(); ?></h2>
-							<?php the_content(); ?>
-						</div>
-					<?php endwhile; wp_reset_postdata(); ?>
-					<!-- show pagination here -->
-				<?php else : ?>
-					<!-- show 404 error here -->
-			<?php endif; ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php 
+    $args = array(
+	'show_option_all'    => '',
+	'orderby'            => 'name',
+	'order'              => 'ASC',
+	'style'              => 'list',
+	'show_count'         => 0,
+	'hide_empty'         => 1,
+	'use_desc_for_title' => 1,
+	'child_of'           => 0,
+	'feed'               => '',
+	'feed_type'          => '',
+	'feed_image'         => '',
+	'exclude'            => '',
+	'exclude_tree'       => '',
+	'include'            => '',
+	'hierarchical'       => 1,
+	'title_li'           => __( 'Categories' ),
+	'show_option_none'   => __( '' ),
+	'number'             => null,
+	'echo'               => 1,
+	'depth'              => 0,
+	'current_category'   => 0,
+	'pad_counts'         => 0,
+	'taxonomy'           => 'category',
+	'walker'             => null
+    );
+    wp_list_categories( $args ); 
+?>
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
