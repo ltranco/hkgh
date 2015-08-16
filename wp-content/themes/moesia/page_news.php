633f41b3ff30ee3fe878f39ce419cb146fdf4e04
Long Tran <v.long128@gmail.com>
2015-08-15 22:12:48 -0700

    trying to query specific posts

--- wp-content/themes/moesia/page_news.php
+++ wp-content/themes/moesia/page_news.php
@@ -14,40 +14,8 @@ Template Name: Full width
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php 
-    $args = array(
-	'show_option_all'    => '',
-	'orderby'            => 'name',
-	'order'              => 'ASC',
-	'style'              => 'list',
-	'show_count'         => 0,
-	'hide_empty'         => 1,
-	'use_desc_for_title' => 1,
-	'child_of'           => 0,
-	'feed'               => '',
-	'feed_type'          => '',
-	'feed_image'         => '',
-	'exclude'            => '',
-	'exclude_tree'       => '',
-	'include'            => '',
-	'hierarchical'       => 1,
-	'title_li'           => __( 'Categories' ),
-	'show_option_none'   => __( '' ),
-	'number'             => null,
-	'echo'               => 1,
-	'depth'              => 0,
-	'current_category'   => 0,
-	'pad_counts'         => 0,
-	'taxonomy'           => 'category',
-	'walker'             => null
-    );
-    wp_list_categories( $args ); 
-?>
-				?>
-
+				<h1>WARNING</h1>
+				
 			<?php endwhile; // end of the loop. ?>
 
 
